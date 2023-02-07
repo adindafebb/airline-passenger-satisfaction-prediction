8cbededc8eea9fa526fac5f1ef24461b40021ca8
Adinda Febby Nuraini <94818522+adindafebb@users.noreply.github.com>
2023-02-07 14:10:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import scikit-learn as sklearn
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 from joblib import dump, load
